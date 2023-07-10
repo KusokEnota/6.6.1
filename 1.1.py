a73cc5a6221734e5fef3bcf41734b9bceeb62129
KusokEnota <enimausy@gmail.com>
2023-07-10 23:57:32 +0300

    dz_add_6.1.1

---
+++ 1.1.py
@@ -0,0 +1,34 @@
+"""В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку."""
+
+import sys
+
+
+__all__ = ['date_validate']
+
+_LEAP = 4
+_END_DAY = 31
+_START_DAY = 1
+_END_MONTH = 12
+_START_MONTH = 1
+_END_YEAR = 9999
+_START_YEAR = 1
+
+
+def date_validate(date: str) -> bool:
+    day, month, year = map(int, date.split('.'))
+    return ((_END_DAY >= day >= _START_DAY) and
+            (_END_MONTH >= month >= _START_MONTH) and
+            (_END_YEAR >= year >= _START_YEAR))
+
+
+def _is_leap_year(date: str) -> bool:
+    day, month, year = map(int, date.split('.'))
+    if year % _LEAP == 0:
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    date = sys.argv[1]   # можно - if len(sys.argv) < 2
+    print(date_validate(date))
+
